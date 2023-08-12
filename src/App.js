fd8de3d6b41662de83d8c892913144cda92483d9
Harsh <2019med1008@iitrpr.ac.in>
2023-08-12 14:09:14 +0530

    Commit by Harsh

--- src/App.js
+++ src/App.js
@@ -1,23 +1,76 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from 'react';
 
-function App() {
+import IconSearch from './search.svg'
+import './App.css'
+import MovieTemplate from './MovieTemplate';
+
+// API key : bb88aa90
+
+const ApiUrl = 'http://www.omdbapi.com?apikey=bb88aa90';
+//const ApiUrl = 'http://www.omdbapi.com/?i=tt3896198&apikey=bb88aa90'
+
+const App = () => {
+
+  const [movies, setMovies] = useState([]);
+
+  const [searchMovie, setsearchMovie] = useState('');
+
+  const searchedMovie = async (searchTitle) => {
+    const response = await fetch(`${ApiUrl}&s=${searchTitle}`);
+    
+    const data = await response.json();
+  
+    setMovies(data.Search);
+  }
+
+  useEffect(() => {
+    searchedMovie('Superman');
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className = "App">
+      
+      <h1>CineVerse</h1>
+
+      <div className = 'Search'>
+        
+        <input
+          placeholder = 'Type in the name of the movie you want to search for'
+          
+          value = {searchMovie}
+
+          onChange = {(event) => setsearchMovie(event.target.value)}
+        />
+
+        <img 
+          src = {IconSearch}
+          
+          alt = 'Search for'
+          
+          onClick = {() => searchedMovie(searchMovie)}
+        />
+
+      </div>
+
+      {
+        movies.length > 0 
+        ? (
+          <div className = 'Container'>
+            
+            {movies.map((movie) => (
+              <MovieTemplate movie={movie} key={movie.imdbID} />
+            ))}
+            
+          </div>
+        ) : (
+          <div className = 'Empty'>
+
+            <h2>No Movies Found</h2>
+          
+          </div>
+        )
+      }
+
     </div>
   );
 }
